39437ef4c1963ede3351a1ee111954b4b7c2438b
ann9cht <ann9cht@proton.me>
2026-09-03 13:05:13 +0700

    updt settings serpan

--- home/programs/hyprland/serpantinum.nix
+++ home/programs/hyprland/serpantinum.nix
@@ -13,33 +13,69 @@
       wallpaperDir = "/home/ann9cht/Wallpapers";
 
       general = {
-        language = "en";
+        location = {
+          latitude = 18.6733;
+          longitude = 105.6813;
+          city = "Vinh";
+          region = "Nghe An";
+          country_name = "Viet Nam";
+          country_code = "VN";
+          timezone = "Asia/Bangkok";
+          source = "manual";
+        };
+        language = "vi";
+        avatarPath = "/home/ann9cht/Pictures/avatar.jpg";
+        muteSfx = false;
+        sfxVolume = 100;
+        quickactions = true;
+        weatherInterval = 15;
         weatherUnit = "metric";
-        weatherInterval = 30;
       };
 
       bar = {
-        position = "top";
-        style = "solid";
-        width = 40;
-        workspaceCount = 8;
-        modules = {
-          left = [ "workspaces" ];
-          center = [ "time" ];
-          right = [ "tray" [ "kb" "wifi" "vol" ] ];
+        position = "left";
+        width = 100;
+        opacity = 100;
+        style = "modular";
+        time = {
+          format = "HH:mm:ss";
         };
-      };
-
-      theme = {
-        fontFamily = "Adwaita Mono";
-        borderRadius = 12;
-        matugen = true;
+        autohide = false;
+        autohideTimeout = 1000;
+        workspaceCount = 5;
+        groupColors = {
+          g_kb = "#a0cafd";
+        };
+        modules = {
+          left = [ "workspaces" "media" ];
+          center = [ "timedate" "info" "weather" ];
+          right = [
+            [ "sysmon" "tray" "wifi" "vol" ]
+          ];
+        };
       };
 
       notifications = {
         dnd = false;
         position = "top right";
         sound = true;
+        soundFile = "${inputs.serpantinum}/src/assets/sounds/notifications/Botanica.wav";
+        showEmptyGraphic = true;
+      };
+
+      theme = {
+        fontFamily = "Noto Sans";
+        borderRadius = 29;
+        activePreset = "Matugen";
+        matugen = true;
+      };
+
+      launcher = {
+        position = "bottom";
+        width = 600;
+        itemCount = 6;
+        terminalCommand = "kitty -e";
+        smartRanking = true;
       };
     };
   };
