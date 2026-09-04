e746412bbe1849b55c25d30c97e813a85c44a2d6
ann9cht <ann9cht@proton.me>
2026-09-04 12:46:58 +0700

    xóa user thừa

--- system/core/user.nix
+++ system/core/user.nix
@@ -2,13 +2,6 @@
 
 {
   programs.fish.enable = true;
-  
-  users.users."ann9cht" = {
-    isNormalUser = true;
-    description = "ann9cht";
-    extraGroups = [ "networkmanager" "wheel" ];
-    shell = pkgs.fish;
-  };
 
   users = {
     users = {
