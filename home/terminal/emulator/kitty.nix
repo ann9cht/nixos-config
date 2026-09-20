71e4868fbe6cf7d9c8df3f607daf6821b59cf990
ann9cht <ann9cht@proton.me>
2026-09-20 23:26:00 +0700

    mờ kitty

--- home/terminal/emulator/kitty.nix
+++ home/terminal/emulator/kitty.nix
@@ -17,7 +17,7 @@
       cursor_shape = "beam";
       cursor_beam_thickness = "1.5";
 
-      background_opacity = "1.0";
+      background_opacity = "0.85";
       confirm_os_window_close = 0;
 
       scrollback_lines = 10000;
