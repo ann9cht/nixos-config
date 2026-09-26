5e6542494b11ede056d77d09181f1768f378067e
ann9cht <ann9cht@proton.me>
2026-09-27 01:05:02 +0700

    waydroi helper

--- system/virtualisation.nix
+++ system/virtualisation.nix
@@ -5,4 +5,11 @@
     enable = true;
     package = pkgs.waydroid-nftables;
   };
+
+  environment.systemPackages = with pkgs; [ waydroid-helper ];
+
+  systemd = {
+    packages = with pkgs; [ waydroid-helper ];
+    services.waydroid-mount.wantedBy = [ "multi-user.target" ];
+  };
 }
