8a963535105b95704dc4caa949e5ec41dc6627ae
ann9cht <ann9cht@proton.me>
2026-09-27 01:41:43 +0700

    cmm kitty 0.49 à

--- home/terminal/emulator/kitty.nix
+++ home/terminal/emulator/kitty.nix
@@ -1,6 +1,7 @@
 {
   programs.kitty = {
     enable = true;
+
     keybindings = {
       "XF86Paste" = "paste_from_clipboard";
     };
@@ -13,31 +14,27 @@
       bold_italic_font = "auto";
       modify_font = "cell_height 110%";
 
-      cursor_trail = 1;
       cursor_shape = "beam";
       cursor_beam_thickness = "1.5";
+      cursor_trail = 1;
 
       background_opacity = "0.85";
+      hide_window_decorations = "yes";
+      window_padding_width = 4;
+      remember_window_size = "no";
       confirm_os_window_close = 0;
 
       scrollback_lines = 10000;
       wheel_scroll_min_lines = 1;
 
       enable_audio_bell = "no";
-      hide_window_decorations = "yes";
-
-      window_padding_width = 4;
-
       copy_on_select = "clipboard";
-
       open_url_with = "default";
-
       notify_on_cmd_finish = "unfocused 10.0";
       shell_integration = "enabled";
     };
 
     extraConfig = ''
-
       include ~/.config/kitty/colors.conf
       background #303446
       cursor #c6a0f6
