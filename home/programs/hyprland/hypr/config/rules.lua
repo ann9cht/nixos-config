64aa0801b46a7881dd88ffcfce08b208b457b9d0
ann9cht <ann9cht@proton.me>
2026-09-26 12:51:35 +0700

    rules

--- home/programs/hyprland/hypr/config/rules.lua
+++ home/programs/hyprland/hypr/config/rules.lua
@@ -1,7 +1,7 @@
 hl.window_rule({match = { class = "protonvpn-app" }, float = true, size = {440, 200}})
-hl.window_rule({match = { class = "org.fcitx." }, float = true, size = {720, 480}})
-hl.window_rule({match = { class = "org.kde.kdeconnect.app" }, float = true, size = {720, 480}})
-
 hl.window_rule({match = { class = "org.gnome.Loupe" }, float = true, size = {"(monitor_w*0.45)", "(monitor_h*0.45)"}})
 hl.window_rule({match = { class = "input-remapper-gtk" }, float = true, size = {"(monitor_w*0.45)", "(monitor_h*0.45)"}})
-hl.window_rule({match = { class = "org.fcitx.Fcitx5.Addon.Lotus.Settings"}, float = true, size = {"(monitor_w*0.45)", "(monitor_h*0.45)"}})
+
+for _, class in ipairs({ "org.fcitx.", "org.fcitx.Fcitx5.Addon.Lotus.Settings" , "org.kde.kdeconnect.app" }) do
+  hl.window_rule({ match = { class = class }, float = true, size = { 750, 500 } })
+end
